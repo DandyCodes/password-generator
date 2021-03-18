7915037922a61b1c42a64dfb409dc7626c6c5b9e
JB <54263206+DandyCodes@users.noreply.github.com>
2021-03-18 05:03:31 +0000

    accomplished js password generation functionaliy

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,43 @@
-// Assignment Code
-let passwordTextArea = document.querySelector('#password');
-let generateButton = document.querySelector('#generate');
-let main = document.querySelector('main');
-let lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
-let upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-let numericCharacters = '0123456789';
-let specialCharacters = ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
-let passwordOptions = {
-  criteria: [],
-  length: randomIntInRange(8, 128),
-  validCharacters: lowerCaseLetters + upperCaseLetters + numericCharacters + specialCharacters
+class PasswordOptions {
+  constructor() {
+    this.length = randomIntInRange(8, 129);
+    this.validCharacters = lowerCaseLetters + upperCaseLetters + numericCharacters + specialCharacters;
+  }
+}
+let passwordOptions = new PasswordOptions;
+const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+const upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numericCharacters = '0123456789';
+const specialCharacters = ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+const promptDiv = createPromptDiv();
+const passwordTextarea = setUpPasswordTextarea();
+const generateButton = setUpGenerateButton();
+readyGenerate();
+
+
+function createPromptDiv() {
+  const promptDiv = document.querySelector('main').appendChild(document.createElement('div'));
+  promptDiv.setAttribute('id', 'prompt');
+  return promptDiv;
 }
 
 
-let promptDiv = main.appendChild(document.createElement('div'));
-promptDiv.setAttribute('id', 'prompt');
-generateButton.addEventListener('click', promptCriteria);
+function setUpPasswordTextarea() {
+  const passwordTextarea = document.querySelector('#password');
+  passwordTextarea.value = '';
+  return passwordTextarea;
+}
+
+
+function setUpGenerateButton() {
+  const generateButton = document.querySelector('#generate');
+  generateButton.addEventListener('click', promptCriteria);
+  return generateButton;
+}
 
 
 function readyGenerate() {
+  passwordOptions = new PasswordOptions;
   generateButton.removeEventListener('click', cancel);
   generateButton.addEventListener('click', promptCriteria);
   generateButton.textContent = "Generate Password";
@@ -37,59 +56,68 @@ function cancel() {
   readyGenerate();
 }
 
+
 function cleanUpPrompt() {
   Array.from(promptDiv.children).forEach(child => {
     child.remove();
   });
 }
 
-// Write password to the #password input
+
+function clearPassword() {
+  passwordTextarea.value = '';
+}
+
 function writePassword() {
-  let password = generatePassword();
-  passwordTextArea.value = password;
+  cleanUpPrompt();
+
+  const password = generatePassword();
+  passwordTextarea.value = password;
+  readyGenerate();
 }
 
 
 function generatePassword() {
   let password = '';
   for (let i = 0; i < passwordOptions.length; i++) {
-    password += passwordOptions.validCharacters[randomIntInRange(0, passwordOptions.validCharacters.length - 1)];
+    password += passwordOptions.validCharacters[randomIntInRange(0, passwordOptions.validCharacters.length)];
   }
   return password;
 }
 
 
 function promptCriteria() {
-  cleanUpPrompt();
+  clearPassword();
   readyCancel();
+  cleanUpPrompt();
 
-  let criteriaDiv = promptDiv.appendChild(document.createElement('div'));
+  const criteriaDiv = promptDiv.appendChild(document.createElement('div'));
   criteriaDiv.appendChild(document.createElement('h3')).textContent = 'Select Criteria';
   criteriaDiv.setAttribute('id', 'criteria');
-  let criteriaContent = criteriaDiv.appendChild(document.createElement('div'));
-  let lengthCheck = criteriaContent.appendChild(document.createElement('input'));
+  const criteriaContent = criteriaDiv.appendChild(document.createElement('div'));
+  const lengthCheck = criteriaContent.appendChild(document.createElement('input'));
   lengthCheck.type = 'checkbox';
   criteriaContent.appendChild(document.createElement('label')).textContent = 'Length';
   criteriaContent.appendChild(document.createElement('br'));
-  let characterCheck = criteriaContent.appendChild(document.createElement('input'));
+  const characterCheck = criteriaContent.appendChild(document.createElement('input'));
   characterCheck.type = 'checkbox';
   criteriaContent.appendChild(document.createElement('label')).textContent = 'Character Type';
   criteriaContent.appendChild(document.createElement('br'));
-  let okButton  = criteriaDiv.appendChild(document.createElement('button'));
+  const okButton  = criteriaDiv.appendChild(document.createElement('button'));
   okButton.addEventListener('click', ok);
   okButton.textContent = 'Ok';
 
   function ok() {
     if (lengthCheck.checked) {
-      passwordOptions.criteria.push('length');
+      passwordOptions.length = false;;
     }
     if (characterCheck.checked) {
-      passwordOptions.criteria.push('characterTypes')
+      passwordOptions.validCharacters = false;
     }
-    if (passwordOptions.criteria.includes('length')) {
+    if (!passwordOptions.length) {
       promptLength();
     }
-    else if (passwordOptions.criteria.includes('characterTypes')) {
+    else if (!passwordOptions.validCharacters) {
       promptCharacterTypes();
     }
     else {
@@ -101,28 +129,27 @@ function promptCriteria() {
 
 function promptLength() {
   cleanUpPrompt();
-  readyCancel();
 
-  let lengthDiv = promptDiv.appendChild(document.createElement('div'));
+  const lengthDiv = promptDiv.appendChild(document.createElement('div'));
   lengthDiv.appendChild(document.createElement('h3')).textContent = 'Specify Length';
   lengthDiv.setAttribute('id', 'length');
-  let lengthContent = lengthDiv.appendChild(document.createElement('div'));
+  const lengthContent = lengthDiv.appendChild(document.createElement('div'));
   lengthContent.appendChild(document.createElement('label')).textContent = '(8 - 128 characters)';
   lengthContent.appendChild(document.createElement('br'));
-  let lengthInput = lengthContent.appendChild(document.createElement('input'));
-  let okButton  = lengthDiv.appendChild(document.createElement('button'));
+  const lengthInput = lengthContent.appendChild(document.createElement('input'));
+  const okButton  = lengthDiv.appendChild(document.createElement('button'));
   okButton.addEventListener('click', ok);
   okButton.textContent = 'Ok';
-  let errorMessage = lengthDiv.appendChild(document.createElement('p'));
+  const errorMessage = lengthDiv.appendChild(document.createElement('p'));
 
   function ok() {
-    let input = lengthInput.value;
+    const input = lengthInput.value;
     if(isNaN(input) || input < 8 || input > 128) {
       errorMessage.textContent = 'Invalid input. Must be a number between 8 and 128';
     }
     else {
       passwordOptions.length = input;
-      if (passwordOptions.criteria.includes('characterTypes')) {
+      if (!passwordOptions.validCharacters) {
         promptCharacterTypes();
       }
       else {
@@ -135,32 +162,31 @@ function promptLength() {
 
 function promptCharacterTypes() {
   cleanUpPrompt();
-  readyCancel();
 
-  let charDiv = promptDiv.appendChild(document.createElement('div'));
-  charDiv.appendChild(document.createElement('h3')).textContent = 'Select Character Types';
-  charDiv.setAttribute('id', 'char');
-  let charContent = charDiv.appendChild(document.createElement('div'));
-  let lowerCheck = charContent.appendChild(document.createElement('input'));
+  const characterDiv = promptDiv.appendChild(document.createElement('div'));
+  characterDiv.appendChild(document.createElement('h3')).textContent = 'Select Character Types';
+  characterDiv.setAttribute('id', 'character-types');
+  const characterContent = characterDiv.appendChild(document.createElement('div'));
+  const lowerCheck = characterContent.appendChild(document.createElement('input'));
   lowerCheck.type = 'checkbox';
-  charContent.appendChild(document.createElement('label')).textContent = 'Lowercase Letters';
-  charContent.appendChild(document.createElement('br'));
-  let upperCheck = charContent.appendChild(document.createElement('input'));
+  characterContent.appendChild(document.createElement('label')).textContent = 'Lowercase Letters';
+  characterContent.appendChild(document.createElement('br'));
+  const upperCheck = characterContent.appendChild(document.createElement('input'));
   upperCheck.type = 'checkbox';
-  charContent.appendChild(document.createElement('label')).textContent = 'Uppercase Letters';
-  charContent.appendChild(document.createElement('br'));
-  let numericCheck = charContent.appendChild(document.createElement('input'));
+  characterContent.appendChild(document.createElement('label')).textContent = 'Uppercase Letters';
+  characterContent.appendChild(document.createElement('br'));
+  const numericCheck = characterContent.appendChild(document.createElement('input'));
   numericCheck.type = 'checkbox';
-  charContent.appendChild(document.createElement('label')).textContent = 'Numeric Characters';
-  charContent.appendChild(document.createElement('br'));
-  let specialCheck = charContent.appendChild(document.createElement('input'));
+  characterContent.appendChild(document.createElement('label')).textContent = 'Numeric Characters';
+  characterContent.appendChild(document.createElement('br'));
+  const specialCheck = characterContent.appendChild(document.createElement('input'));
   specialCheck.type = 'checkbox';
-  charContent.appendChild(document.createElement('label')).textContent = 'Special Characters';
-  charContent.appendChild(document.createElement('br'));
-  let okButton  = criteriaDiv.appendChild(document.createElement('button'));
+  characterContent.appendChild(document.createElement('label')).textContent = 'Special Characters';
+  characterContent.appendChild(document.createElement('br'));
+  const okButton  = characterDiv.appendChild(document.createElement('button'));
   okButton.addEventListener('click', ok);
   okButton.textContent = 'Ok';
-  let errorMessage = lengthDiv.appendChild(document.createElement('p'));
+  const errorMessage = characterDiv.appendChild(document.createElement('p'));
 
   function ok() {
     if (!(lowerCheck.checked || upperCheck.checked || numericCheck.checked || specialCheck.checked)) {
@@ -185,7 +211,9 @@ function promptCharacterTypes() {
   }
 }
 
-
+/**
+ * Returns a random integer between the `min`(inclusive) and the `max`(exclusive).
+ */
 function randomIntInRange(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
